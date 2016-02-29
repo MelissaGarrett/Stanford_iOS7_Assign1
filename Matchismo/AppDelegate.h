53550b4b1abe4b9ddb794b244e5733af0a4e8297
MelissaGarrett <melissa_garrett@hotmail.com>
2016-02-29 09:18:35 -0600

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Melissa  Garrett on 2/29/16.
+//  Copyright © 2016 MelissaGarrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
